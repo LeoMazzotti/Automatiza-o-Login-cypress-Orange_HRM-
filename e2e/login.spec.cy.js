d72ebd36bc2b451e73a318392d4240f18e3715d4
Leonardo Mazzotti <leonardo.mazzotti@yahoo.com.br>
2025-03-02 14:33:16 -0300

    Adicionei lista de seletores para a login spec

--- e2e/login.spec.cy.js
+++ e2e/login.spec.cy.js
@@ -1,22 +1,32 @@
 describe('Orango HRM Testes', () => {
+  
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField:"[name='password']",
+    loginButton:"[type='submit']",
+    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+    wrongCredentialAlert: "[role='alert']"
+    }
+  
+  
   it('Login - Success', () => { //podemos colocar o ".skip" depois do it para pular esse teste => it.slip(...)
     cy.visit(
       'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
     )
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('[type="submit"]').click()
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // para verificar se a rota está correta
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard') // para verificar sem contem uma palavra específica
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard') // para verificar sem contem uma palavra específica
   })
     it('Login - Fail', () => {
       cy.visit(
         'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
       )
-      cy.get('[name="username"]').type('Admina')
-      cy.get('[name="password"]').type('admin123456')
-      cy.get('[type="submit"]').click()
-      cy.get('[role="alert"]')
+      cy.get(selectorsList.usernameField).type('Admina')
+      cy.get(selectorsList.passwordField).type('admin123456')
+      cy.get(selectorsList.loginButton).click()
+      cy.get(selectorsList.wrongCredentialAlert)
       
       
 
